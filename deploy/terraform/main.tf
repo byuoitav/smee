e636862b2f6420fbc9d3dcba76eb14f05725e17d
Daniel Randall <dgrandall@me.com>
2021-06-01 13:21:22 -0600

    main tf

---
+++ deploy/terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-storage-586877430255"
+    dynamodb_table = "terraform-state-lock-586877430255"
+    region         = "us-west-2"
+
+    // THIS MUST BE UNIQUE
+    key = "smee-v2.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ssm_parameter" "eks_cluster_endpoint" {
+  name = "/eks/av-cluster-endpoint"
+}
+
+provider "kubernetes" {
+  host = data.aws_ssm_parameter.eks_cluster_endpoint.value
+}
